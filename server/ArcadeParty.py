ed34804265f3aaf5556b81bc2f0c8c98fdd3ae98
jenh <jen@sevenminuteserver.com>
2019-10-15 22:07:48 -0500

    .

--- server/ArcadeParty.py
+++ server/ArcadeParty.py
@@ -1,39 +1,44 @@
 import logging
 import os, re
-#import boto3
 from werkzeug.contrib.cache import RedisCache
-
-#dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-
-#table = dynamodb.Table('arcade_session')
 
 from datetime import datetime
 
 from flask import Flask, json, abort 
 from flask_ask import Ask, question, statement, audio, current_stream, logger, session, request, convert_errors, context
 
+# Set up our Flask application
 application = Flask(__name__)
-ask = Ask(application, "/",stream_cache=RedisCache(key_prefix="arcamb_",default_timeout=172800))
+
+# Connect to Redis, create the default key prefix. We set the timeout to 172800 seconds, which is 48 hours. You can change as needed.
+ask = Ask(application, "/",stream_cache=RedisCache(key_prefix="arcparty_",default_timeout=172800))
+
+# Configure logging. We're logging to /tmp/arcade.log; you can change this and the logging format to whatever you like.
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 FORMAT = '%(asctime)s - %(module)s - %(levelname)s - Thread_name: %(threadName)s - %(message)s'
 logging.basicConfig(
     format=FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p',
     filename='/tmp/arcade.log', level=logging.DEBUG)
 
-year_cache = RedisCache(key_prefix="arcamb_year_",default_timeout=172800)
+# Set up a key to store the last year played. Key times out after 48 hours
+year_cache = RedisCache(key_prefix="arcparty",default_timeout=172800)
 
+# This is what we do when an Alexa session initiates, we start logging and reject any connection that doesn't send the proper appid (change your_app_id to your Alexa app id:
 @ask.on_session_started
 def start_session():
     logging.debug("Session started at {}".format(datetime.now().isoformat()))
     appid = str(session.application.applicationId)
-    if appid != "amzn1.ask.skill.ce2004da-9cac-476d-b108-56733868eda6":
+    if appid != "your_app_id":
       abort(403)
     else:
         pass
 
+# This is what we do at launch, prompt the user to give us a year to play
 @ask.launch
 def handle_launch():
     return question("Welcome to Arcade Party. You can be transported to an arcade in 1981, 83, 86, or 92. Which do you choose?").reprompt("I'm sorry, I didn't get that. Say 1981, 1983, 1986, or 1992 to play Arcade Party")
+
+# This is the meat of our skill, given a year, we play a corresponding audio file. We set up a session, save the session info to cache and play the correct audio file (or cancel/exit/quit/stop if somehow they ended up being sent to the PlayIntent). 
 
 @ask.intent('PlayIntent', default={'year': 'noidea'})
 def handle_playintent(year):
@@ -41,10 +46,6 @@ def handle_playintent(year):
     sessionid = str(session.sessionId)
     userid = str(session.user.userId)
     year_cache.set(userid, plainyear)
-    #if year is 'noidea':
-    #    playurl = "https://archive.org/download/ArcadeAmbience/arcade86.mp3"
-    #    speech = "Sorry, I didn't get that. I'm sure you want to feel like you're in an arcade, so I'll play sounds of 1986 for you!"
-     #   return audio(speech).play(playurl) 
     if year is 'exit' or year is 'cancel' or year is 'exit' or year is 'stop' or year is 'quit':
         return statement("Shutting down your Arcade Party. Thanks for playing!")
     elif plainyear == "1981" or plainyear == "81":
@@ -66,14 +67,17 @@ def handle_playintent(year):
     else:
         return question("Sorry, I  didn't quite get that. Would you like arcade ambiance from 1981, 1983, 1986, or 1992?")
 
+# When Alexa sends us a stop command, here's what we do:
 @ask.intent('AMAZON.StopIntent')
 def handle_stop():
     return audio("Thanks for playing!").stop()
 
+# When Alexa sends us a cancel command, here's what we do:
 @ask.intent('AMAZON.CancelIntent')
 def handle_cancel():
     return audio("Thanks for playing!").stop()
 
+# When Alexa sends us a next command, we figure out where we are, then jump to the next track by invoking the PlayIntent with the next year:
 @ask.intent('AMAZON.NextIntent')
 def handle_next():
     userid = str(session.user.userId)
@@ -91,6 +95,7 @@ def handle_next():
         year = 92
     return handle_playintent(year)
 
+# If Alexa sends us a previous intent, we find the current track and play the last track by invoking the PlayIntent with the previous year.
 @ask.intent('AMAZON.PreviousIntent')
 def handle_back():
     userid = str(session.user.userId)
@@ -108,27 +113,32 @@ def handle_back():
         year = 83
     return handle_playintent(year)
 
+# Basic help response:
 @ask.intent('AMAZON.HelpIntent')
 def handle_help():
     return question("Welcome to Arcade Party. Would you like to be audibly transported to an arcade in 1981, 1983, 1986, or 1992?")
 
+# Relaunch:
 @ask.intent('AMAZON.StartOverIntent')
 def start_over():
     return handle_launch()
 
+# Pause:
 @ask.intent('AMAZON.PauseIntent')
 def handle_pause():
     return audio("Stopping. Say Start to resume.").stop()
 
+# Resume:
 @ask.intent('AMAZON.ResumeIntent')
 def handle_resume():
     return audio("Resuming playback.").resume()
 
+# End the session, return 200:
 @ask.session_ended
 def session_ended():
     return "{}", 200
 
-
+# Our main routine, :
 if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
